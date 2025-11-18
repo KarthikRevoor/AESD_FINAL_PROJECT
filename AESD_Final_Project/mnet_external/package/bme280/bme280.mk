cba39caafce94f747b6c2d241d013f095abbb80f
kaif-shahid-shaikh <kash7211@colorado.edu>
2025-11-17 23:02:03 -0700

    Add BME280 driver + config fixes + buildroot defconfig updates

---
+++ AESD_Final_Project/mnet_external/package/bme280/bme280.mk
@@ -0,0 +1,23 @@
+################################################################################
+#BME280 package description
+################################################################################
+
+BME280_VERSION = 1.0
+BME280_SITE = $(BR2_EXTERNAL_MNET_EXTERNAL_PATH)/package/bme280/src
+BME280_SITE_METHOD = local
+
+define BME280_BUILD_CMDS
+    $(MAKE) -C $(LINUX_DIR) \
+        M=$(@D) \
+        ARCH=$(KERNEL_ARCH) \
+        CROSS_COMPILE="$(TARGET_CROSS)" \
+        modules
+endef
+
+define BME280_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 644 $(@D)/bme280.ko \
+        $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/misc/bme280.ko
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
