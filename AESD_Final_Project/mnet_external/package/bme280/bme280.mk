bf5bc4fcc939f4cc0dcd01a16640b6ee0db2b106
Karthik Revoor <karthik.r@colorado.edu>
2025-11-19 16:06:28 -0700

    changed the repo since its the application and changed the .mk file

--- AESD_Final_Project/mnet_external/package/bme280/bme280.mk
+++ AESD_Final_Project/mnet_external/package/bme280/bme280.mk
@@ -1,11 +1,24 @@
-################################################################################
-# bme280 kernel module package
-################################################################################
+##############################################################
+#
+# BME280 Kernel Module (GitHub source)
+#
+##############################################################
 
-BME280_VERSION = 1.0
-BME280_SITE = $(BR2_EXTERNAL_MNET_EXTERNAL_PATH)/package/bme280/src
-BME280_SITE_METHOD = local
+# Commit hash you want Buildroot to checkout
+BME280_VERSION = a7c91b92e34f3b4e6e7f93c75ff128c322ab1234
 
+# GitHub SSH URL (recommended so Buildroot uses your SSH key)
+# MUST be ssh format when private repo or when using CI
+BME280_SITE = git@github.com:<username>/<repo>.git
+
+BME280_SITE_METHOD = git
+# Set to YES only if repo contains submodules
+BME280_GIT_SUBMODULES = NO
+
+
+##############################################################
+# Build the kernel module
+##############################################################
 define BME280_BUILD_CMDS
 	$(MAKE) -C $(LINUX_DIR) \
 		M=$(@D) \
@@ -14,11 +27,19 @@ define BME280_BUILD_CMDS
 		modules
 endef
 
+
+##############################################################
+# Install the kernel module on target rootfs
+##############################################################
 define BME280_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 644 $(@D)/bme280.ko \
 		$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/misc/bme280.ko
 endef
 
+
+##############################################################
+# Evaluate package type
+##############################################################
 $(eval $(kernel-module))
 $(eval $(generic-package))
 
