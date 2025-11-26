23a1756bc6d03334308b641f5a8fa09996423406
Karthik Revoor <karthik.r@colorado.edu>
2025-11-26 00:20:58 -0700

    made them remote repositories and verified the working

--- AESD_Final_Project/mnet_external/package/bme280/bme280.mk
+++ AESD_Final_Project/mnet_external/package/bme280/bme280.mk
@@ -1,24 +1,45 @@
-################################################################################
-# bme280 kernel module package
-################################################################################
+##############################################################
+#
+# BME280 Kernel Module (GitHub source)
+#
+##############################################################
 
-BME280_VERSION = 1.0
-BME280_SITE = $(BR2_EXTERNAL_MNET_EXTERNAL_PATH)/package/bme280/src
-BME280_SITE_METHOD = local
+# Commit hash you want Buildroot to checkout
+BME280_VERSION = 8326d3a86655c5e66f45eac99740d8a433157836
 
+
+BME280_SITE = git@github.com:KarthikRevoor/mnet-apps.git
+
+BME280_SITE_METHOD = git
+# Set to YES only if repo contains submodules
+BME280_GIT_SUBMODULES = NO
+BME280_MODULE_SUBDIRS = bme280/src
+
+
+##############################################################
+# Build the kernel module
+##############################################################
 define BME280_BUILD_CMDS
 	$(MAKE) -C $(LINUX_DIR) \
-		M=$(@D) \
+		M=$(@D/bme280/src) \
 		ARCH=$(KERNEL_ARCH) \
 		CROSS_COMPILE="$(TARGET_CROSS)" \
 		modules
 endef
 
+
+##############################################################
+# Install the kernel module on target rootfs
+##############################################################
 define BME280_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 644 $(@D)/bme280.ko \
+	$(INSTALL) -D -m 644 $(@D)/bme280/src/bme280.ko \
 		$(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/kernel/drivers/misc/bme280.ko
 endef
 
+
+##############################################################
+# Evaluate package type
+##############################################################
 $(eval $(kernel-module))
 $(eval $(generic-package))
 
